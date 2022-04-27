d5044706c3145969461c24efebb46b986fd618d2
illy skalikxyb <pagany10@gmail.com>
2022-04-27 21:39:39 +0200

    kode1 done

--- IlyaGitHub_3Project/Source.cpp
+++ IlyaGitHub_3Project/Source.cpp
@@ -1,7 +1,25 @@
-
+#define INT
+#include "Lib.h"
 
 int main()
 {
-	
+	int s = 10;
+#ifdef INT
+	int* arr = new int[s];
+#endif
+	int num;
+
+	init_random(arr, s);
+	cout << "Generated numbers:\n";
+	print(arr, s);
+	cout << "\nMax: " << getmax(arr, s);
+	cout << "\nMin: " << getmin(arr, s);
+	cout << "\n\nSort:\n";
+	sort(arr, s);
+	print(arr, s);
+	cout << "\nEnter index(0,1,2...9): ";
+	cin >> num;
+	change(arr, s, num);
+	cout << endl;
 	return 0;
 }
