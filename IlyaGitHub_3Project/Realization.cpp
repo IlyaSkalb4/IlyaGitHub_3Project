756faeb4c54603e2dc3c1dfb0e81ebb3ff352c23
illy skalikxyb <pagany10@gmail.com>
2022-04-28 11:43:32 +0200

    Add source/Done code.

--- IlyaGitHub_3Project/Realization.cpp
+++ IlyaGitHub_3Project/Realization.cpp
@@ -9,27 +9,6 @@ Cret_b* creat(int s)
 	Cret_b* q = new Cret_b[s];
 	return q;
 }
-void print(Cret_b book)
-{
-	cout << book.name << "\t" << book.aut << "\t" << book.stud << "\t" << book.genre;
-	cout << endl;
-}
-void print(Cret_b* book, int s)
-{
-	for (size_t i = 0; i < s; i++)
-	{
-		cout << "#" << i + 1 << " ";
-		print(book[i]);
-	}
-}
-void coppy(char* book)
-{
-	char tmp[30];
-	int tp = 0;
-	gets_s(tmp, 30);
-	tp = strlen(tmp) + 1;
-	strcpy_s(book, tp, tmp);
-}
 Cret_b input()
 {
 	Cret_b book;
@@ -43,6 +22,57 @@ Cret_b input()
 	cout << "Æàíð: ";
 	coppy(book.genre);
 	return book;
+}
+Cret_b* add(Cret_b* books, int& s, Cret_b book)
+{
+	Cret_b* tmp = creat(s + 1);
+	for (size_t i = 0; i < s; i++)
+	{
+		tmp[i] = books[i];
+	}
+	tmp[s] = book;
+	del(books);
+	s++;
+	return tmp;
+}
+Cret_b* delet(Cret_b* books, int& s, char* name)
+{
+	Cret_b* tmp = creat(s - 1);
+	int j = 0;
+	for (size_t i = 0; i < s; i++)
+	{
+		if (strcmp(name, books[i].name) == 0)
+		{
+			continue;
+		}
+		tmp[j] = books[i];
+		j++;
+	}
+	del(books);
+	s--;
+	return tmp;
+}
+void print(Cret_b book)
+{
+	cout << book.name << "\t" << book.aut << "\t" << book.stud << "\t" << book.genre;
+	cout << endl;
+}
+void print(Cret_b* book, int s)
+{
+	for (size_t i = 0; i < s; i++)
+	{
+		cout << "#" << i + 1 << " ";
+		print(book[i]);
+	}
+	cout << endl;
+}
+void coppy(char* book)
+{
+	char tmp[30];
+	int tp = 0;
+	gets_s(tmp, 30);
+	tp = strlen(tmp) + 1;
+	strcpy_s(book, tp, tmp);
 }
 void searchAut(Cret_b* books, int s, char* tmp)
 {
@@ -103,33 +133,4 @@ void sortName(Cret_b* books, int s)
 		}
 		books[j + 1] = temp;
 	}
-}
-Cret_b* add(Cret_b* books, int& s, Cret_b book)
-{
-	Cret_b* tmp = creat(s + 1);
-	for (size_t i = 0; i < s; i++)
-	{
-		tmp[i] = books[i];
-	}
-	tmp[s] = book;
-	del(books);
-	s++;
-	return tmp;
-}
-Cret_b* delet(Cret_b* books, int& s, char* name)
-{
-	Cret_b* tmp = creat(s - 1);
-	int j = 0;
-	for (size_t i = 0; i < s; i++)
-	{
-		if (strcmp(name, books[i].name) == 0)
-		{
-			continue;
-		}
-		tmp[j] = books[i];
-		j++;
-	}
-	del(books);
-	s--;
-	return tmp;
 }
--- IlyaGitHub_3Project/Source.cpp
+++ IlyaGitHub_3Project/Source.cpp
@@ -1,9 +1,90 @@
-#include <iostream>
+#include "prototype.h"
 
 using namespace std;
 
 int main()
 {
-
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int s = 10, q;
+	char ch[30], a;
+	Cret_b* books = creat(s);
+	books[0] = { "Ремонт_ПК", "Мюллер", "Вильямс", "Справочник" },
+		books[1] = { "Язык_програмирования_С", "Керниган", "Вильямс", "Учебник" },
+		books[2] = { "Java_8", "Шилдт", "Oracle Press", "Руководство" },
+		books[3] = { "Head_First_Java", "Sierra", "Oreilly", "Учебник" },
+		books[4] = { "Google_Android", "Голощапов", "БХВ-Петербург", "Учебник" },
+		books[5] = { "Справочное_руководство_по_С", "Страустрап", "Бином", "Справочник" },
+		books[6] = { "Head_First Android", "Гриффитс", "Oreilly", "Учебник" },
+		books[7] = { "Язык_програмирования_С", "Прата", "Вильямс", "Лекции" },
+		books[8] = { "Стандарт_С++", "Чупринов", "Интерстрон", "Справочник" },
+		books[9] = { "Ява_и_С++", "Мартин", "Диалектика", "Справочник" };
+	print(books, s);
+	do {
+		cout << "Изменить книгу по номеру - 1\nВывод - 2\nПоиск по автору - 3\nПоиск по имени - 4\nСортировка по автору - 5\nСортировка по имени 6\n"\
+			"Добавить книгу - 7\nУдалить книгу по названию - 8\nВыход - 0\n";
+		cout << "Выберите вариант: ";
+		cin >> a;
+		system("cls");
+		cin.ignore();
+		switch (a)
+		{
+		case '1':
+		{
+			cout << "Выберите номер книги: ";
+			cin >> q;
+			books[q - 1] = input();
+			break;
+		}
+		case '2':
+		{
+			print(books, s);
+			break;
+		}
+		case '3':
+		{
+			cout << "Введите имя автора: ";
+			gets_s(ch, 30);
+			searchAut(books, s, ch);
+			break;
+		}
+		case '4':
+		{
+			cout << "Введите название книги: ";
+			gets_s(ch, 30);
+			searchName(books, s, ch);
+			break;
+		}
+		case '5':
+		{
+			sortAut(books, s);
+			break;
+		}
+		case '6':
+		{
+			sortName(books, s);
+			break;
+		}
+		case '7':
+		{
+			Cret_b book = input();
+			books = add(books, s, book);
+			break;
+		}
+		case '8':
+		{
+			cout << "Введите название книги: ";
+			gets_s(ch, 30);
+			books = delet(books, s, ch);
+			break;
+		}
+		default:
+			if (a != '0')
+			{
+				cout << "invalid\n";
+			}
+		}
+	} while (a != '0');
+	del(books);
 	return 0;
 }
